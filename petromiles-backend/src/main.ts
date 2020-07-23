e9559ea2170c2b30af3b65512d94b0e1036713e8
mecoccaro <mecoccaro.16@est.ucab.edu.ve>
2020-07-23 13:04:58 -0400

    Travis file

--- petromiles-backend/src/main.ts
+++ petromiles-backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as helmet from 'helmet';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
